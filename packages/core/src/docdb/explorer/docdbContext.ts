fc20508f3f34e81e4402fc8ff27d120e3313a263
Mark Cooke <mark.cooke@improving.com>
2024-07-29 16:16:45 -0500

    Update node context values

--- packages/core/src/docdb/explorer/docdbContext.ts
+++ packages/core/src/docdb/explorer/docdbContext.ts
@@ -4,11 +4,11 @@
  */
 
 export const DocDBContext = {
-    Cluster: 'awsDocDB.cluster',
-    ClusterRunning: 'awsDocDB.cluster.running',
-    ClusterStopped: 'awsDocDB.cluster.stopped',
-    Instance: 'awsDocDB.instance',
-    InstanceAvailable: 'awsDocDB.instance.available',
+    Cluster: 'awsDocDB-cluster',
+    ClusterRunning: 'awsDocDB-cluster-running',
+    ClusterStopped: 'awsDocDB-cluster-stopped',
+    Instance: 'awsDocDB-instance',
+    InstanceAvailable: 'awsDocDB-instance-available',
 } as const
 
 export type DocDBNodeContext = (typeof DocDBContext)[keyof typeof DocDBContext]
