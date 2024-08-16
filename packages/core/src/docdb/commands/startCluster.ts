5b416e233d02bf5f42c9d628f80821e8190d52df
Mark Cooke <mark.cooke@improving.com>
2024-08-16 10:50:48 -0500

    split classes into separate files

--- packages/core/src/docdb/commands/startCluster.ts
+++ packages/core/src/docdb/commands/startCluster.ts
@@ -21,16 +21,3 @@ export function startCluster(node?: DBClusterNode): Promise<void> {
         }
     })
 }
-
-export function stopCluster(node?: DBClusterNode): Promise<void> {
-    return telemetry.docdb_stopCluster.run(async () => {
-        if (node?.arn && node?.regionCode) {
-            await node.client.stopCluster(node.arn)
-            getLogger().info('Stop cluster: %O', node.name)
-            void vscode.window.showInformationMessage(
-                localize('AWS.docdb.stopCluster.success', 'Stopping cluster: {0}', node.name)
-            )
-            node?.parent.refresh()
-        }
-    })
-}
